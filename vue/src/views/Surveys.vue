b389fc8baa4c30ecf478f81fe574a15e90db1284
abbas-mastan <abbasmastan2424@gmail.com>
2023-11-24 10:02:20 +0500

    routes protected

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -17,7 +17,7 @@
         <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div class="lg:pr-4">
             <div class="lg:max-w-lg">
-              <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Dashboard</h1>
+              <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">All Surveys</h1>
               <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
             </div>
           </div>
